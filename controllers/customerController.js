74e257ff766e1592ce006e1a32ea56a755b648fb
Jay200123 <kenken.ayonon@gmail.com>
2023-04-26 18:57:02 +0800

    updateCustomer

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -76,6 +76,34 @@ exports.getOneCustomer = async(req, res)=>{
     }
 }// end for getOneCustomer
 
+exports.updateCustomer = async(req, res)=>{
+
+    try{
+        const data = await new Promise((resolve, reject)=>{
+
+            const customerId = req.params.id;
+
+            const { title, fname, lname, addressline, town, zipcode, phone, creditlimit, level } = req.body;
+            const values = [title, fname, lname, addressline, town, zipcode, phone, creditlimit, level, customerId ];
+
+            const sql = "UPDATE customers SET title=?, fname=?, lname=?, addressline=?, town=?, zipcode=?, phone=?, creditlimit=?, level=? WHERE customer_id=?";
+
+            con.query(sql, values, (err, result)=>{
+
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(result);
+                }
+            });
+        });//end for promise
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+}//end for updateCustomer
+
 exports.deleteCustomer = async(req, res)=>{
 
     try{
--- routes/customers.js
+++ routes/customers.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getCustomers, storeCustomer, getOneCustomer, deleteCustomer } = require('../controllers/customerController');
+const { getCustomers, storeCustomer, getOneCustomer, deleteCustomer, updateCustomer } = require('../controllers/customerController');
 
 router.get('/customers', getCustomers);
 router.post('/customers/store', storeCustomer);
-router.get('/customers/:id', getOneCustomer);
+router.get('/customers/edit/:id', getOneCustomer);
+router.put('/customers/update/:id', updateCustomer);
 router.delete('/customers/:id', deleteCustomer);
 
 module.exports = router;
